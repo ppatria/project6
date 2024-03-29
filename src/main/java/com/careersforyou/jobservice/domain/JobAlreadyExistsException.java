ba4025c8531447c13b94b70761d9d07ca7df9b1f
ppatria <prodeopatria51@gmail.com>
2024-03-28 21:41:26 -0600

    Updated the validation constraint error messages in Job.java

--- src/main/java/com/careersforyou/jobservice/domain/JobAlreadyExistsException.java
+++ src/main/java/com/careersforyou/jobservice/domain/JobAlreadyExistsException.java
@@ -1,2 +1,7 @@
-package com.careersforyou.jobservice.domain;public class JobAlreadyExists {
+package com.careersforyou.jobservice.domain;
+
+public class JobAlreadyExistsException extends RuntimeException {
+    public JobAlreadyExistsException(String jobid) {
+        super("A job with Jobid " + jobid + " already exists.");
+    }
 }
--- src/main/java/com/careersforyou/jobservice/domain/JobNotFoundException.java
+++ src/main/java/com/careersforyou/jobservice/domain/JobNotFoundException.java
@@ -1,2 +1,7 @@
-package com.careersforyou.jobservice.domain;public class JobNotFoundException {
+package com.careersforyou.jobservice.domain;
+
+public class JobNotFoundException extends RuntimeException {
+    public JobNotFoundException(String jobid) {
+        super("The job with Jobid " + jobid + " was not found.");
+    }
 }
